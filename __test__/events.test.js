e5dad31a9c4413ff4cf59301b24ccd62827dff6d
ibrahemomari <ibrahem.omari96@gmail.com>
2021-08-24 23:49:11 +0300

    lap2

--- __test__/events.test.js
+++
@@ -1,9 +0,0 @@
-  
-"use strict";
-const Events=require('events');
-const events=new Events();
-describe("Node Module", () => {
-  it("events", () => {
-    expect(events).toBeInstanceOf(Events);
-  });
-});
---
+++ hub.js
@@ -0,0 +1,56 @@
+"use strict";
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+const io = require("socket.io")(PORT);
+const uuid = require("uuid").v4;
+
+io.on("connection", (socket) => {
+  console.log("CONNECTION SUCCESSFULLY:", socket.id);
+});
+
+const caps = io.of("/caps");
+
+const queue = {
+  messagesId: {},
+};
+
+caps.on("connection", (socket) => {
+  socket.on("join", (room) => {
+    console.log("room name:", room);
+    socket.join(room);
+  });
+
+  socket.on("pickup", (payload) => {
+    const id = uuid();
+    queue.messagesId[id] = { event: "pickup", payload };
+    caps.emit("pickup", { id, payload: queue.messagesId[id] });
+  });
+
+  socket.on("in-transit", (payload) => {
+    const id = uuid();
+    queue.messagesId[id] = { event: "in-transit", payload };
+    caps
+      .to(payload.store)
+      .emit("in-transit", { id, payload: queue.messagesId[id] });
+  });
+
+  socket.on("delivered", (payload) => {
+    const id = uuid();
+    queue.messagesId[id] = { event: "delivered", payload };
+    caps
+      .to(payload.store)
+      .emit("delivered", { id, payload: queue.messagesId[id] });
+  });
+
+  socket.on("get-all", (payload) => {
+    Object.keys(queue.messagesId).forEach((id) => {
+      socket.emit("message", { id, payload: queue.messagesId[id] });
+    });
+  });
+
+  socket.on("received", (id) => {
+    delete queue.messagesId[id];
+  });
+});
